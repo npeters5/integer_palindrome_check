abcfd69806ff1d42fd54a161efe8e2da85ece528
Nora Peters <norapeters5@gmail.com>
2018-03-29 22:56:04 -0700

    7/8 tests passing. still troubleshooting

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,23 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number.nil? || number < 0
+  return true if number.size == 1 || number == 0
+  while number >= 10
+    first_num = number
+    i = 0
+    until first_num < 10
+      first_num /= 10
+      i += 1
+    # end
+    last_num = number % 10
+    number = (number % 10**i) / 10
+    end
+    if first_num != last_num
+      false
+    end
+  end
+  true
 end
+
+
+# time complexity is O(n)
